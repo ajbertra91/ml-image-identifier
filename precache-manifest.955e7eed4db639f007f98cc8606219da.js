630e52540da38e7c2d86feb4fc07de90de63c83c
Adam Bertrand <ajbertra91@gmail.com>
2022-07-07 15:36:10 -0400

    Updates

---
+++ precache-manifest.955e7eed4db639f007f98cc8606219da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2de0d47c1962da0485b",
+    "url": "/static/css/main.4847413b.chunk.css"
+  },
+  {
+    "revision": "d2de0d47c1962da0485b",
+    "url": "/static/js/main.f498f1b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "314b97ebff46a6dd0926",
+    "url": "/static/js/2.1c6a099c.chunk.js"
+  },
+  {
+    "revision": "b879da62134304d07293be29de55c0af",
+    "url": "/index.html"
+  }
+];
